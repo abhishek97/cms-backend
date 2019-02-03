b90b6db5d8a3a1a9c3ab0e4b5c69a1604a5d4957
abhishek97 <zapper.awake@gmail.com>
2019-02-04 01:47:00 +0530

    clear technical debt

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'cms-backend',
+      script    : 'index.js',
+      env: {
+        DB_USER: 'alphanet',
+        DB_PASS: 'UKeDJVNF295lTc1n',
+        DB_HOST: '139.59.6.227',
+        SMS_URI: 'http://198.24.149.4/API/pushsms.aspx',
+        SMS_SENDERID: 'ALPHNT',
+        FRONTEND: 'http://localhost:4200',
+        DB_NAME: 'alphanet_dev'
+
+      },
+      env_production : {
+        NODE_ENV: 'production',
+        SMS_LOGINID: 'mohitdel94',
+        SMS_PASS: 'kirk@metalx',
+        DB_NAME: 'alphanet',
+        FRONTEND: 'https://cms.alphanetbroadband.com'
+      }
+    }
+  ]
+};
